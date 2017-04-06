9fa5dc8b296aeb09ce8866a9c394137348540807
David Abad Quijada <david.abad@tantacom.com>
2017-04-06 09:01:25 +0200

    Ajustes varios para la maquetacion del site:
    - Se ha usado bootstrap y el principio mobile first
    - Módulos nuevos
    - Generación de suggestions
    - Plugins jQuery nuevos
    - Generacion de CSS propio de la plantilla
    - Modificacion de comportamientos por defecto de Drupal (template.php)
    - Integración del blog en el site

--- sites/all/modules/accesibilidadweb_contact_form/templates/accesibilidadweb-contact-form-block.tpl.php
+++ sites/all/modules/accesibilidadweb_contact_form/templates/accesibilidadweb-contact-form-block.tpl.php
@@ -1,6 +1,6 @@
 <div id="">
-  <h4><?php echo $title; ?></h4>
-  <div class="form-wrapper">
+  <h3 class="text-center"><?php echo $title; ?></h3>
+  <div class="form-wrapper col-md-6 col-md-offset-3">
     <?php echo $form; ?>
   </div>
 
---
+++ sites/all/themes/tanta_zen/templates/region--header.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * Overwrite sites/all/themes/zen/templates/block.tpl.php
+ *
+ */
+?>
+<?php print $content; ?>
