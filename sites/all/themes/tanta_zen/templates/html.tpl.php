3765aeb588757880dece2e2d2f3967fc4829f991
David Abad <davidabad@Front09.grupoonetec.local>
2017-08-16 11:58:35 +0200

    [INV-200]: actualizacion modulo eu cookie compliance

--- sites/all/themes/tanta_zen/templates/html.tpl.php
+++ sites/all/themes/tanta_zen/templates/html.tpl.php
@@ -49,7 +49,7 @@
   <?php endif; ?>
   <?php print $page_top; ?>
   <?php print $page; ?>
-  <?php print $page_bottom; ?>
   <?php print $scripts; ?>
+  <?php print $page_bottom; ?>
 </body>
 </html>
